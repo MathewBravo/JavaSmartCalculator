67343b50d3ebd6c8349aecec73dd7c24e732e2ff
Mathew Bravo <mathewbravo94@gmail.com>
2022-09-14 11:54:16 -0400

    Implemented detection for multi subtraction

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -1,10 +1,10 @@
 package calculator;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import javax.swing.text.StyleContext;
+import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 public class Main {
 
@@ -16,103 +16,143 @@ public class Main {
 
         String userInput = "";
         userInput = scanner.nextLine();
-        while(!userInput.equals("/exit")){
+        while (!userInput.equals("/exit")) {
             ActionProcessor(userInput);
             userInput = scanner.nextLine();
         }
         System.out.println("Bye!");
     }
 
-    public static void ActionProcessor(String userInput){
-        if(userInput.length() == 0){
-            return;
-        } else if(userInput.equals("/help")){
-            System.out.println("The program calculates the sum of numbers");
-            return;
-        } else if(userInput.charAt(0) == '/'){
-            System.out.println("Unknown command");
-            return;
-        }else if(userVariables.containsKey(userInput)){
-            System.out.println(userVariables.get(userInput));
+    public static void ActionProcessor(String userInput) {
+        if(userInput.equals("")){
             return;
         }
-        else if(!userInput.contains("=") && userInput.matches("[a-zA-Z]+")){
-            System.out.println("Unknown variable");
+        if(!validInput(userInput)){
             return;
         }
+        if(userInput.charAt(0) == '/'){
+            commandHandler(userInput);
+        }else if (userInput.contains("=")){
+            variableAssignment(userInput);
+        } else{
+            if(userInput.matches(".*[a-zA-Z]+.*")){
+                userInput = variableProcessor(userInput);
+            }
+            processEquation(userInput);
+        }
+    }
 
-        if(userInput.contains("=")){
-            String[] splitInput = userInput.split("=");
-            for(int i = 0; i < splitInput.length; i++){
-                splitInput[i] = splitInput[i].trim();
-            }
-            if(splitInput.length > 2){
-                System.out.println("Invalid assignment");
-                return;
-            }
-            userVariable(splitInput);
+    public static String determineSubstraction(String subtractionIndicator){
+        if(!subtractionIndicator.contains("-")){
+            return subtractionIndicator;
+        }
+        String[] splitIndicator = subtractionIndicator.split("[^-]");
+        if(splitIndicator[1].length() % 2 == 0){
+            return subtractionIndicator.replace(splitIndicator[1], "+");
         }else{
-            String[] splitInput = userInput.split(" ");
-            for(int i = 0; i < splitInput.length; i++ ){
-                if(userVariables.containsKey(splitInput[i])){
-                    splitInput[i] = String.valueOf(userVariables.get(splitInput[i]));
-                }
-            }
-            System.out.println(calculate(splitInput));
-        }
-
-    }
-
-    public static Character determineOperator(String operator){
-        if(operator.length() % 2 == 0 && operator.charAt(0) == '-'){
-            return '+';
-        }else{
-            return operator.charAt(0);
+            return subtractionIndicator.replace(splitIndicator[1], "-");
         }
     }
-    public static Integer calculate(String[] requestedCalculation){
-        int num = Integer.parseInt(requestedCalculation[0]);
-        char operator = ' ';
-        for(int i = 1; i < requestedCalculation.length; i++){
-            if(requestedCalculation[i].contains("+")){
-                operator = determineOperator(requestedCalculation[i]);
-                continue;
-            }
-            else if(requestedCalculation[i].contains("-")) {
-                operator = determineOperator(requestedCalculation[i]);
-                continue;
-            } else {
-                if(operator == '+'){
-                    num += Integer.parseInt(requestedCalculation[i]);
-                }else if(operator == '-'){
-                    num -= Integer.parseInt(requestedCalculation[i]);
-                }
-            }
+
+
+    public static String mathProcessor(String toBeProcessed){
+        if(toBeProcessed.contains("--")){
+            toBeProcessed = determineSubstraction(toBeProcessed);
         }
-        return num;
+        //2+3
+        String[] splitEquation = toBeProcessed.split("[+\\-/*]");
+        if (splitEquation.length ==1){
+            return toBeProcessed;
+        }
+        int result = 0;
+        if(toBeProcessed.contains("+")){
+            result = Integer.parseInt(splitEquation[0]) + Integer.parseInt(splitEquation[1]);
+        } else if (toBeProcessed.contains("-")) {
+            result = Integer.parseInt(splitEquation[0]) - Integer.parseInt(splitEquation[1]);
+        } else if (toBeProcessed.contains("*")) {
+            result = Integer.parseInt(splitEquation[0]) * Integer.parseInt(splitEquation[1]);
+        } else if (toBeProcessed.contains("/")) {
+            result = Integer.parseInt(splitEquation[0]) / Integer.parseInt(splitEquation[1]);
+        }
+        return String.valueOf(result);
     }
 
-    public static void userVariable(String[] userDeclaration){
-        if(!validateIdentifier(userDeclaration[0]))
-        {
-            System.out.println("Invalid identifier");
-            return;
-        }else if(!userVariables.containsKey(userDeclaration[1]) && userDeclaration[1].matches(".*[a-z].*"))
-        {
+    public static boolean hasBrackets(String userInput){
+        return userInput.contains("(");
+    }
+    public static void processEquation(String userInput){
+        //"\\++"
+        while (hasBrackets(userInput)){
+            int rightBracketIndex = userInput.indexOf(")");
+            int leftBracketIndex = userInput.lastIndexOf("(", rightBracketIndex);
+            String tobeProcessed = userInput.substring(leftBracketIndex, rightBracketIndex+1);
+            System.out.println(tobeProcessed);
+            String result = mathProcessor(tobeProcessed.substring(1, tobeProcessed.length()-1));
+            userInput = userInput.replace(tobeProcessed, result);
+        }
+        userInput = mathProcessor(userInput);
+        System.out.println(userInput);
+    }
+    public static void variableAssignment(String userInput){
+        userInput = userInput.replace(" ", "");
+        String[] variableSetter = userInput.split("=");
+        if(variableSetter.length > 2) {
+            System.out.println("Invalid variable");
+        }else if(variableSetter[1].matches("[a-zA-Z]")){
             System.out.println("Invalid assignment");
-            return;
-        }
-        if(userDeclaration.length > 2) {
-            throw new IllegalArgumentException("Array too long");
-        }
-        if(userVariables.containsKey(userDeclaration[1])){
-            userVariables.put(userDeclaration[0], userVariables.get(userDeclaration[1]));
         }else{
-            userVariables.put(userDeclaration[0], Integer.parseInt(userDeclaration[1]));
+            userVariables.put(variableSetter[0], Integer.parseInt(variableSetter[1]));
         }
     }
 
-    public static boolean validateIdentifier(String identifier){
-        return identifier.matches("[a-zA-Z]+");
+    public static String variableProcessor(String userInput){
+        String[] splitEquation = userInput.split("[+\\-/*()]");
+        for(int i = 0; i < splitEquation.length; i++){
+            if (userVariables.containsKey(splitEquation[i])){
+                int value = userVariables.get(splitEquation[i]);
+                userInput = userInput.replace(splitEquation[i], String.valueOf(value));
+            }
+        }
+        return userInput;
+    }
+    public static boolean validInput(String userInput){
+        if(userInput.charAt(0) == '/'){
+            if(userInput.contains("exit")
+                    || userInput.contains("help")
+            ){
+                return true;
+            }else{
+                System.out.println("Unknown command");
+                return false;
+            }
+        } else if ((userInput.contains("(") && !userInput.contains(")"))) {
+            System.out.println("Invalid expression");
+            return false;
+        } else if((userInput.contains("(") && userInput.contains(")"))){
+            int leftBracket = patternOccurrence("\\(", userInput);
+            int rightBracket = patternOccurrence("\\)", userInput);
+            if(leftBracket != rightBracket){
+                System.out.println("Invalid expression");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int patternOccurrence(String regex, String inputString){
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(inputString);
+        int counter = 0;
+        while (matcher.find()){
+            counter++;
+        }
+        return counter;
+    }
+
+    public static void commandHandler(String userInput){
+        if(userInput.equals("/help")){
+            System.out.println("The program calculates the sum of numbers");
+        }
     }
 }
+
